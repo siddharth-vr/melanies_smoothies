0e0c305f929d029c88c8a1482dc50dbdc4a14383
Siddharth <56465916+siddharth-vr@users.noreply.github.com>
2025-05-26 15:45:10 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 # sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = TRUE)
 
