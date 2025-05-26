a1b63f4b966df6027bcfbc5ca3a14bdde0bd1bc9
Siddharth <56465916+siddharth-vr@users.noreply.github.com>
2025-05-26 15:42:23 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-
-smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/orange")
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = TRUE)
 
